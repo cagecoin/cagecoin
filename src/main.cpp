66cec3afe9c50fa8a59df30d4e29d45399f8ce70
cagecoin <randym3@web.de>
2013-12-29 12:37:00 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2092,7 +2092,7 @@ bool LoadBlockIndex(bool fAllowNew)
 		//   vMerkleTree: 6f80efd038 
 
         // Genesis block
-        const char* pszTimestamp = "Nintondo";
+        const char* pszTimestamp = "Nicolas Cage";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
